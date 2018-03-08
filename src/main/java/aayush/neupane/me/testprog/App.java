f11fa49312fb9153ddb992b8affecd4da1497e8d
Aayush Neupane <aayush_Alex@yahoo.com>
2018-03-07 22:38:04 -0800

    init

---
+++ src/main/java/aayush/neupane/me/testprog/App.java
@@ -0,0 +1,13 @@
+package aayush.neupane.me.testprog;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
